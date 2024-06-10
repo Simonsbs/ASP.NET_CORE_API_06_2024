e0d04c68458238b8c209854ec7b50e2ce0d6f8e2
Simon B.Stirling <simonsbs@gmail.com>
2024-06-10 16:13:37 +0300

    .

--- ShopAPI/Models/ProductForCreationDTO.cs
+++ ShopAPI/Models/ProductForCreationDTO.cs
@@ -1,7 +1,13 @@
-﻿namespace ShopAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopAPI.Models;
 
 public class ProductForCreationDTO {
+	[Required]
+	[MaxLength(100)]
 	public string Name { get; set; }
-	public string Description { get; set; }
+
+	[MaxLength(200)]
+	public string? Description { get; set; }
 }
 

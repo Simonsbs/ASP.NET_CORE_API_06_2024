4f2abb6dcb602c0c929f099d464778d733a2dfa2
Simon B.Stirling <simonsbs@gmail.com>
2024-06-13 15:58:33 +0300

    .

---
+++ ShopAPI/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopAPI.Entities;
+
+public class Product {
+	[Key]
+	public int ID { get; set; }
+
+	[Required]
+	[MaxLength(100)]
+	public string Name { get; set; }
+
+	[MaxLength(200)]
+	public string? Description { get; set; }
+}
+
+
+public class Category {
+	[Key]
+	public int ID { get; set; }
+
+	[Required]
+	[MaxLength(100)]
+	public string Name { get; set; }
+
+	[MaxLength(200)]
+	public string Description { get; set; }
+}

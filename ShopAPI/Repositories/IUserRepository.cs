f175d96aa573b95c5cf687e16f28b1d566ecc1ab
Simon B.Stirling <simonsbs@gmail.com>
2024-06-18 13:21:26 +0300

    .

---
+++ ShopAPI/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAPI.Contexts;
+using ShopAPI.Entities;
+
+namespace ShopAPI.Repositories;
+
+public interface IUserRepository {
+	Task<User?> AuthenticateUserAsync(string username, string password);
+}
+
+class UserRepository(MyDbContext _db) : IUserRepository {
+	public async Task<User?> AuthenticateUserAsync(string username, string password) {
+		return await _db.Users
+			.FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+	}
+}
